779655305502279d71ab66a7bded7c45c579648a
Tony Le <letony@icloud.com>
2019-03-22 03:25:00 +1100

    More architectural planning

--- Architecture.cpp
+++ Architecture.cpp
@@ -1,6 +1,7 @@
 namespace MinityEngine
 {
     class string;
+    std::vector<GameObject*> gameObjects;
 
     class CoreEngine
     {
@@ -22,7 +23,13 @@ namespace MinityEngine
 
         void CoreUpdate()
         {
-            Update();
+            Update()
+            {
+                for (auto go : gameObjects)
+                {
+                    go->update();
+                }
+            }
         }
 
         void CoreDraw()
@@ -38,9 +45,49 @@ namespace MinityEngine
 
     }
 
+ 
+
     class GameObject
     {
+        std::string m_name;
+        std::vector<Component> components;
+        Scene* m_scene = nullptr;
 
+        GameObject()
+        {
+            addComponent(new Transform());
+        }
+
+        void addedToScene(const Scene& scene)
+        {
+            m_scene = scene;
+        }
+
+        void addComponent(Component component)
+        {
+            components.push_back(component);
+            component.addedToGameObject(this);
+        }
+
+        void update()
+        {
+            //Run update on all components
+
+        }
+
+    private:
+        void draw() //Should this be hidden from the user???
+        {
+            //Loop through all components and draw if necessary
+            for (auto c : components)
+            {
+                //If the component has a renderer component then draw that component?
+                if (c->draw() != null)
+                {
+                    c->draw();
+                }
+            }
+        }
     }
     
     class Object;
@@ -52,11 +99,27 @@ namespace MinityEngine
         static Object Instantiate();
     }
 
-    class GameObject : Object
+    /////////
+    class Scene
+    {
+        std::vector<GameObject> gameObjects;
+        EventDispatcher* m_eventDispatcher;
+        SceneRender* m_renderer;
+    }
+
+
+    //////////
+
+    class GameObject //: Object
     {
     private:
         std::vector<Component> m_components;
     public:
+        GameObject()
+        {
+
+        }
+
         int layer;
         bool active;
         bool isStatic;
@@ -64,44 +127,73 @@ namespace MinityEngine
     
     }
 
-    class Component : Object
+    class Component
+    {
+        virtual void Start() {}
+        virtual void Update() {}
+        void Draw() {}
+        void End() {}
+    }
+
+    class Transform : Component
     {
     public:
-        GameObject gameObject;
-        string tag;
-        Component rigidbody;
-        Component camera;
-        Component light;
-        Component animation;
-        Component renderer;
-        Component audio;
-        Component collider;
-        Component particleSystem;
-
-        Transform transform;
-
-        void BroadcastMessage();
-        void SendMessage();
-        void GetComponent();
-
+        glm::vec3 position;
+        glm::vec3 rotation;
+        glm::vec3 scale;
     }
 
     class Behaviour : Component
     {
     public:
         bool enabled;
+
+        void Update() override {} //?
+
     }
 
-    class MonoBehaviour : Behaviour
+    class PlayerBehaviour : Behaviour
     {
-    public:
-        bool useGUILayout;
-        bool runInEditMode;
-        static void print(const char *);
-        void CancelInvoke();
-        void Invoke();
-        Coroutine StartCoroutine();
+        void Update() override
+        {
+            
+        }
     }
+
+    // class Component : Object
+    // {
+    // public:
+    //     GameObject gameObject;
+    //     string tag;
+    //     Component rigidbody;
+    //     Component camera;
+    //     Component light;
+    //     Component animation;
+    //     Component renderer;
+    //     Component audio;
+    //     Component collider;
+    //     Component particleSystem;
+
+    //     Transform transform;
+
+    //     void BroadcastMessage();
+    //     void SendMessage();
+    //     void GetComponent();
+
+    // }
+
+
+
+    // class MonoBehaviour : Behaviour
+    // {
+    // public:
+    //     bool useGUILayout;
+    //     bool runInEditMode;
+    //     static void print(const char *);
+    //     void CancelInvoke();
+    //     void Invoke();
+    //     Coroutine StartCoroutine();
+    // }
 }
 
 using namespace MinityEngine;
